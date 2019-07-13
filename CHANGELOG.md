89c36952e44e146d80f6153ea872096c4930189b
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2019-07-13 16:17:40 +0200

    see Changelog

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.7.4] - 2019-07-13
+### Changed
+- `ipa.py` Make IPA field a `list` (support multiple IPA transcriptions for one word)
+
+### Fixed
+- `ipa.py` Improved IPA parsing
+
 ## [0.7.3] - 2019-05-29
 ### Fixed
 - `pos.py` Prevent duplicate POS names
--- wiktionary_de_parser/methods/ipa.py
+++ wiktionary_de_parser/methods/ipa.py
@@ -1,27 +1,56 @@
 import re
+from pdb import set_trace as bp
+
+"""
+Reference: https://de.wiktionary.org/wiki/Hilfe:Aussprache
+
+The WiktionaryDe community decided in 2015 to only add IPA transcriptions
+for the lemma (and not for inflected forms; these are on a seperate page).
+Unfortunately there are still many pages with inflected forms.
+For example: "Schweizerdeutsch" and Singular 2 "(das) Schweizerdeutsche":
+    - :{{IPA}} {{Lautschrift|ˈʃvaɪ̯t͡sɐˌdɔɪ̯t͡ʃ}}, {{Sg.2}} {{Lautschrift|ˈʃvaɪ̯t͡sɐˌdɔɪ̯t͡ʃə}}
+
+Sometimes they are just seperated by comma and no additional info about the grammatical form is present:
+    Example "Aserbaidschanisch" and Singular 2 "(das) Aserbaidschanische":
+    - :{{IPA}} {{Lautschrift|ˌazɐbaɪ̯ˈd͡ʒaːnɪʃ}}, {{Lautschrift|ˌazɐbaɪ̯ˈd͡ʒaːnɪʃə}}
+
+In the last example we can't say whether these are just two different IPA
+transcriptions for the same word or for two different words. 
+
+Temporary solution:
+Keep the first entry. Check if the last IPA letter of every other entry is the same
+as the one in the first match (because it's likely that the last character doesn't
+change in different IPA transcriptions for the same word)
+
+"""
+
+vowels = r'aɪ̯|aʊ̯|ɛɪ̯|ɔɪ̯|ʊɪ̯|aː|eː|ɛː|iː|oː|øː|õː|uː|yː|ɨ|i̯|ʉ|ɯ|ɪ|ʏ|ʊ|ø|ɑ|ɘ|ɵ|ɤ|ə|ɛ|œ|ɜ|ɞ|ʌ|ɔ|æ|ɐ|ɶ|ɒ|ã|ɐ̯|a|e|i|o|u|y'
+consonants = r'ʈ|ɖ|ɟ|ɢ|ʔ|ɸ|β|v|θ|ð|ʃ|ʒ|ʂ|ʐ|ç|ʝ|l̩|ɣ|χ|ʁ|ħ|ʕ|ɦ|ɬ|ɮ|ɱ|m̩|ɱ̍|ɱ̩|n̩|ɳ|ɲ|ŋ|ŋ̍|ŋ̩|ɴ|ʙ|ʀ|ⱱ|ɾ|ʦ|ʧ|ʤ|ɽ|ɺ|ʋ|ɹ|ɻ|ɰ|ɭ|ʎ|ʟ|p|b|t|d|c|k|ɡ|q|f|s|z|x|h|m|n|r|l|j|ɫ'
+ipa_letters_re = re.compile(r'(' + vowels + '|' + consonants + ')')
 
 
 def init(title, text, current_record):
-    match_firstline = re.search(r'({{Aussprache}}\n[^\n]+)', text)
-    if not match_firstline:
-        return False
-
-    first_line = match_firstline.group(1)
-    found_ipa = re.findall(r'{{Lautschrift\|([^}]+)}}', first_line)
+    match_ipa = re.findall(r'{{Lautschrift\|([^}]+)}}', text)
+    found_ipa = [x.strip() for x in match_ipa if x != '…' and x.strip() != '']
     if not found_ipa:
         return False
 
-    result = found_ipa[0]
-
-    # compare with ryme ipa field
-    # prioritize ipa value that is part of a rhyme
-    reim_match = re.search(r'{{Reim\|([^}|]+)(?:\|[^}]+)*}}', text)
-    if reim_match:
-        r = reim_match.group(1)
-        m = [x for x in found_ipa if x.endswith(r)]
-        if m:
-            result = m[0]
-
-    result = result.strip()
+    wanted_last_ipa_char = None
+    result = [found_ipa[0]]
+    for idx, ipa_str in enumerate(found_ipa):
+        try:
+            last_ipa_char = ipa_letters_re.findall(ipa_str)[-1]
+        except IndexError:
+            if idx == 0:
+                return False
+            else:
+                break
+        if idx == 0:
+            wanted_last_ipa_char = last_ipa_char
+            continue
+        if last_ipa_char == wanted_last_ipa_char and ipa_str not in result:
+            result.append(ipa_str)
+        else:
+            break
 
     return {'ipa': result}
