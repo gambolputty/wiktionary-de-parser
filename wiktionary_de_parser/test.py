3aa137e157d974311b9c043102cd5ebd782c3c4c
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2021-11-10 22:19:33 +0100

    convert to python poetry

--- wiktionary_de_parser/test.py
+++ wiktionary_de_parser/test.py
@@ -1,7 +1,7 @@
 import os
 from os import name as os_name
 import sys
-from bz2file import BZ2File
+from bz2 import BZ2File
 from pdb import set_trace as bp
 from pprint import pprint
 from __init__ import Parser
@@ -9,10 +9,7 @@ from __init__ import Parser
 # add parent dir to PATH
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
 
-if os_name == 'nt':
-    bzfile_path = 'C:/Users/Gregor/Downloads/dewiktionary-latest-pages-articles-multistream.xml.bz2'
-else:
-    bzfile_path = '/Users/gregor/Downloads/dewiktionary-latest-pages-articles-multistream.xml.bz2'
+bzfile_path = '/mnt/c/Users/gregor/Downloads/dewiktionary-latest-pages-articles-multistream.xml.bz2'
 bz = BZ2File(bzfile_path)
 collection = set()
 for record in Parser(bz):
