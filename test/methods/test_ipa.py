ecad612ed1227d0d8239bde1846fa272534bccb1
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2022-07-18 21:40:14 +0200

    Add method to parse rhymes & tests

--- test/methods/test_ipa.py
+++ test/methods/test_ipa.py
@@ -1,14 +1,19 @@
 import pytest
 
-from wiktionary_de_parser.methods.ipa import init, parse_ipa_strings
-from test.test_data.ipa_data import data
+from wiktionary_de_parser.methods.ipa import init, parse_ipa_strings, parse_rhymes
+from test.test_data.ipa_data import ipa_test_data, rhymes_test_data
 
 
 class TestIPAParsing:
     def test_returns_false(self):
         assert init("test", "test", {}) == False
 
-    @pytest.mark.parametrize("test_input,expected", data)
+    @pytest.mark.parametrize("test_input,expected", ipa_test_data)
     def test_parsing_ipa_strings(self, test_input, expected):
         parse_result = parse_ipa_strings(test_input)
+        assert parse_result == expected
+
+    @pytest.mark.parametrize("test_input,expected", rhymes_test_data)
+    def test_parsing_rhymes(self, test_input, expected):
+        parse_result = parse_rhymes(test_input)
         assert parse_result == expected
