7e5760c217ab1be1a393342f6c7be38911b4f3e6
Gregor Weichbrodt <gregorweichbrodt@gmail.com>
2024-02-04 19:24:34 +0100

    create nested model for lemma and language values

--- wiktionary_de_parser/models.py
+++ wiktionary_de_parser/models.py
@@ -18,16 +18,27 @@ class WiktionaryPageEntry(BaseModel):
     model_config = ConfigDict(arbitrary_types_allowed=True)
 
 
+class Language(BaseModel):
+    lang: str
+    lang_code: str | None
+
+
+class Lemma(BaseModel):
+    lemma: str
+    inflected: bool
+
+
 ParseFlexionResult = dict | None
 ParseIpaResult = list[str] | None
-ParseLanuageResult = dict[str, str | None]
-ParseLemmaResult = dict[str, str | bool]
+ParseLanuageResult = Language
+ParseLemmaResult = Lemma
 ParsePosResult = dict[str, list[str]] | None
 ParseRhymesResult = list[str] | None
 ParseSyllablesResult = list[str] | None
 
 
 class ParsedWiktionaryPageEntry(BaseModel):
+    name: str
     flexion: ParseFlexionResult
     ipa: ParseIpaResult
     language: ParseLanuageResult
